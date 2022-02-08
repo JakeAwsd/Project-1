b16b5be73e8cc8e767428a985443663249984eaf
JakeAwsd <93892085+JakeAwsd@users.noreply.github.com>
2022-02-07 21:13:02 -0600

    Merge branch 'main' of https://github.com/JakeAwsd/Project-1 into js-start-branch

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,21 +1,24 @@
-var priceEl = document.querySelector('#2');
+// var ethprice = document.querySelector('#price-container');
+var apiUrl = 'https://api.etherscan.io/api?module=stats&action=ethprice&apikey=' + apiKey;
+var apiKey = 'YAMMU36XYW1SCRJTKPY8KM9T5EFJC82EX5';
 
-function getApi() {
-    var apiUrl = 'https://api.etherscan.io/api?module=stats&action=ethprice&apikey=' + apiKey;
-    
-    fetch(apiUrl, {
-    method: 'GET', //GET is the default.
-    credentials: 'same-origin', // include, *same-origin, omit
-    redirect: 'follow', // manual, *follow, error
-        })
+var getApi = function (eth) {
+
+    fetch(apiUrl)
+        
         .then(function (response) {
         return response.json();
         })
         .then(function (data) {
             console.log(data)
+                const pricedata = data.result.ethusd
+                document.getElementById('#price-container').textContent = pricedata + " USD";
         })
         .catch(function (error) {
             console.log(error)
         });
+
     }
+
 getApi();
+
